d8334877acafb66e410811f19d3b4db69a9bd389
fernstgruber <fabianernstgruber@hotmail.com>
2017-05-22 18:13:19 +0200

    Add files via upload

--- scripts_2017/nachbarschaftsuntersuchung_geoeinheiten.R
+++ scripts_2017/nachbarschaftsuntersuchung_geoeinheiten.R
@@ -36,6 +36,14 @@ names(geounits_neighbors) <- c(names(geounits_neighbors)[1:3], "Abbr_left")
 geounits_neighbors <- merge(geounits_neighbors,geounits_min, by.x="right",by.y="cat",all.x=T)
 names(geounits_neighbors)[5] <- "Abbr_right"
 geounits_neighbors<- na.omit(geounits_neighbors)
-geounits_neighbors$bordertype <- as.factor(paste(geounits_neighbors$Abbr_left,"_vs_",geounits_neighbors$Abbr_right))
+geounits_ne][ighbors$bordertype <- as.factor(paste(geounits_neighbors$Abbr_left,"_vs_",geounits_neighbors$Abbr_right))
 borderlength <- as.data.frame(tapply(X=geounits_neighbors$length, INDEX=geounits_neighbors$bordertype,FUN=sum))
 write.table(borderlength,file="/home/fabs/Data/paper2_lenny/results_2017/borderlength.txt",sep="\t",row.names=T)
+
+#####inzwischen in excel bearbeiten und summen der geoeinheiten berechnen, dann
+bordertable <- read.table("/home/fabs/Data/paper2_lenny/results_2017/sgu_borderlengths.txt",sep="\t",header=T,row.names = 1)
+bordertable <- bordertable/1000
+percentable <- bordertable/sum(bordertable,na.rm=T)*100
+require(xtable)
+xtable(bordertable,caption = "Length in kilometers of the borders of adjacent SGUs",label = "table:borderlength",digits = 1)
+xtable(percentable,caption = "Percent of the borders of adjacent SGUs",label = "table:borderlength",digits = 2)
